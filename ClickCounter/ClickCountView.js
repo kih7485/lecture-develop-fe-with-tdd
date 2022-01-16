40acd235a3ff65f3dde26d6846f0bc0edba44ed3
inhan <korea4127@gmail.com>
2022-01-16 14:53:06 +0900

    클릭카운트 모듈

---
+++ ClickCounter/ClickCountView.js
@@ -0,0 +1,9 @@
+var App = App || {};
+App.ClickCountView = (clickCounter, updateEl) => {
+  if (!clickCounter) throw Error("clickCounter");
+  return {
+    updateView() {
+      updateEl.innerHTML = clickCounter.getValue();
+    },
+  };
+};
